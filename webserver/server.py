e303b552f26a7cbbef9d0a58981d510c4a99ac8b
Carlo Provinciali <carloprovinciali@gmail.com>
2019-04-16 09:42:41 -0400

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -208,7 +208,7 @@ def productdetails(id):
                     JOIN breweries ON beers.brewer_id = breweries.id 
                     JOIN hops ON beers.hop_id = hops.id 
                     LEFT JOIN (SELECT beer_id, AVG(score) as avg_rating FROM ratings GROUP BY beer_id) r
-                    ON beers.id = ratings.beer_id
+                    ON beers.id = r.beer_id
                     WHERE beers.id=:ID;
            """
     cursor = g.conn.execute(text(cmd), ID=id)
